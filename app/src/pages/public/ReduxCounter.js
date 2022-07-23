3d99ee53961c9b43161f1eb8b1d1ba162c699526
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-07-23 11:39:36 +0200

    yea

--- app/src/pages/public/ReduxCounter.js
+++ app/src/pages/public/ReduxCounter.js
@@ -1,10 +1,13 @@
 import { Counter } from '../../components';
 
+import logo from '../../logo.svg';
+
 export { ReduxCounter };
 
 function ReduxCounter() {
   return (
     <div className="content">
+      <img src={logo} className="App-logo" alt="logo" />
       <Counter />
     </div>
   );
