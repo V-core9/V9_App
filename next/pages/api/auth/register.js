f04fb70b8f6a691439f605317bc83b0d3cfc7a23
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-09-18 10:27:56 +0200

    yea

--- next/pages/api/auth/register.js
+++ next/pages/api/auth/register.js
@@ -1,37 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { uuid as uuidv4 } from 'uuidv4';
 
-import { createUserByEmailAndPassword } from '../../../services/users';
+import { createUserByEmailAndPassword, findUserByEmail } from '../../../services/users';
 
 export default async function handler(req, res) {
-  const { method } = req;
-
-  switch (method) {
-    case "POST":
-      const { email, password } = req.body;
-      if (!email || !password) {
-        res.status(400);
-        throw new Error('You must provide an email and a password.');
-      }
-
-      const existingUser = await findUserByEmail(email);
-
-      if (existingUser) {
-        res.status(400);
-        throw new Error('Email already in use.');
-      }
-
-      const username = req.body.username || uuidv4();
-
-      const user = await createUserByEmailAndPassword({ email, password, username });
-
-      res.status(200).json({
-        id: user.id,
-        username: user.username,
-      });
-      break;
-
-    default:
-      res.status(400).json({ message: 'Invalid method, only POST is allowed.' });
-      break;
+  if (req.method !== 'POST') {
+    res.status(405).send({ message: 'Only POST requests allowed' });
+    return;
   }
+
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({ message: 'You must provide an email and a password.' });
+  }
+
+  const existingUser = await findUserByEmail(email);
+
+  if (existingUser) {
+    res.status(400).json({ message: 'Email already in use.' });
+  }
+
+  const username = req.body.username || uuidv4();
+
+  const user = await createUserByEmailAndPassword({ email, password, username });
+
+  res.status(200).json({
+    id: user.id,
+    username: user.username,
+  });
+
 }
